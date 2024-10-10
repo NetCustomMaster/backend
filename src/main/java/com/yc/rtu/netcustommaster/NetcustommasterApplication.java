e7165c3bd9169aa8a36c3f99bd52bcaac4237f26
RyuTaeUung <aser77992@gmail.com>
2024-10-10 11:27:52 +0900

    feat : start project

---
+++ src/main/java/com/yc/rtu/netcustommaster/NetcustommasterApplication.java
@@ -0,0 +1,13 @@
+package com.yc.rtu.netcustommaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetcustommasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetcustommasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yc/rtu/netcustommaster/NetcustommasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.rtu.netcustommaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetcustommasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
