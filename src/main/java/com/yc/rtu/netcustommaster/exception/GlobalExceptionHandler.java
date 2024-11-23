a9cd0fb675afdba851a4f9b7a2233b13f10d0d84
ParkHyeonseul <132830988+gamecomics@users.noreply.github.com>
2024-11-23 23:03:12 +0900

    대역폭 변경 수정

---
+++ src/main/java/com/yc/rtu/netcustommaster/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.yc.rtu.netcustommaster.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.async.AsyncRequestNotUsableException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AsyncRequestNotUsableException.class)
+    public void handleAsyncException() {
+        // 로깅 또는 처리
+    }
+}
